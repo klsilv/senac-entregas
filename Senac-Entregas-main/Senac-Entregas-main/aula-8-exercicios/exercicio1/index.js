bd9765a0e50395f7e6785b9fb763fd60b8598ddc
klsilv <166620091+klsilv@users.noreply.github.com>
2024-04-29 00:38:19 -0300

    Delete Senac-Entregas-main/Senac-Entregas-main/aula-8-exercicios directory

--- Senac-Entregas-main/Senac-Entregas-main/aula-8-exercicios/exercicio1/index.js
+++
@@ -1,5 +0,0 @@
-const idade = Number(prompt('Quantos anos você tem?'))
-const melhorAmigoIdade = Number(prompt('Quantos anos tem o seu melhor amigo(a)?'))
-
-console.log('Sua idade é maior do que a do seu melhor amigo?', idade > melhorAmigoIdade)
-console.log(amigo - idade)
