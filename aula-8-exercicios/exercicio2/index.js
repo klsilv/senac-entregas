c00964b179038675eb1402877068eec7557ba82b
klsilv <166620091+klsilv@users.noreply.github.com>
2024-04-29 00:19:58 -0300

    Delete aula-8-exercicios directory

--- aula-8-exercicios/exercicio2/index.js
+++
@@ -1,4 +0,0 @@
-let par = Number(prompt('Digite um número par.'))
-console.log('O resto da divisão é: ', par % 2) 
-//O resto da divisão retorna 0 quando um número par é utilizado.
-//O resto da divisão retorna 1 quanto um número impar é utilizado.
