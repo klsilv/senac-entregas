69e47c654de13a1ccc0617213f99226f31eec759
klsilv <166620091+klsilv@users.noreply.github.com>
2024-04-29 00:38:32 -0300

    Delete Senac-Entregas-main/Senac-Entregas-main/aula-10-exercicios directory

--- Senac-Entregas-main/Senac-Entregas-main/aula-10-exercicios/exercicio-1/index.js
+++
@@ -1,6 +0,0 @@
-
-const nome = prompt('Qual é o seu nome?')
-const email = prompt('Digite o seu e-mail.').toLowerCase()
-
-
-console.log(`O email ${email} foi cadastrado com sucesso. Seja bem-vindo(a), ${nome}`)
